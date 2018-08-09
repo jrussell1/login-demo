97d5b0fc4b27e0f06ce789302638b881589e89e4
Jordan Russell <jordanrussell1@gmail.com>
2018-08-09 19:46:01 -0400

    update app.js

--- public/app.js
+++ public/app.js
@@ -127,17 +127,6 @@ var handleSignedInUser = function(user) {
   document.getElementById('phone').textContent = user.phoneNumber;
 
   writeUserData(user.uid, user.name, user.email, user.imageUrl);
-
-   //attach user to Realtime DB 
-   var database = firebase.database();
-
-   function writeUserData() {
-     firebase.database().ref('users/' + userId).set({
-       username: name,
-       email: email,
-       profile_picture : imageUrl
-     });
-   }
   
   
   if (user.photoURL){
@@ -233,3 +222,14 @@ var initApp = function() {
 };
 
 window.addEventListener('load', initApp);
+
+   //attach user to Realtime DB 
+   var database = firebase.database();
+
+   function writeUserData() {
+     firebase.database().ref('users/' + userId).set({
+       username: name,
+       email: email,
+       profile_picture : imageUrl
+     });
+   }
