a135e2ffaac6a5b225058652d911e3b6f65f33e8
Jordan Russell <jordanrussell1@gmail.com>
2018-08-09 18:13:48 -0400

    updated app.js

--- public/app.js
+++ public/app.js
@@ -218,12 +218,27 @@ var initApp = function() {
 
 window.addEventListener('load', initApp);
 
-      //attach user to Realtime DB 
-      var database = firebase.database();
-
-      function writeUserData(name, email) {
-        firebase.database().ref('user/' + userId).set({
+      function writeUserData(userId, name, email, imageUrl) {
+        firebase.database().ref('users/' + userId).set({
           username: name,
           email: email,
-        })();
+          profile_picture : imageUrl
+        });
+      }
+      // [END rtdb_write_new_user]
+      
+      function writeUserDataWithCompletion(userId, name, email, imageUrl) {
+        // [START rtdb_write_new_user_completion]
+        firebase.database().ref('users/' + userId).set({
+          username: name,
+          email: email,
+          profile_picture : imageUrl
+        }, function(error) {
+          if (error) {
+            // The write failed...
+          } else {
+            // Data saved successfully!
+          }
+        });
+        // [START rtdb_write_new_user_completion]
       }
