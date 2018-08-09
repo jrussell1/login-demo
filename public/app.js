b018f163eb7c60fe866174f147cf257e3d72c3ec
Jordan Russell <jordanrussell1@gmail.com>
2018-08-09 17:15:12 -0400

    updated js

--- public/app.js
+++ public/app.js
@@ -123,6 +123,7 @@ var handleSignedInUser = function(user) {
   document.getElementById('name').textContent = user.displayName;
   document.getElementById('email').textContent = user.email;
   document.getElementById('phone').textContent = user.phoneNumber;
+
   
   
   if (user.photoURL){
