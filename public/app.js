3aa1498ac791bdeb0f58e4a6c9c09b14c4410b6a
Jordan Russell <jordanrussell1@gmail.com>
2018-08-09 16:49:20 -0400

    update app.js

--- public/app.js
+++ public/app.js
@@ -139,18 +139,9 @@ var handleSignedInUser = function(user) {
     document.getElementById('photo').style.display = 'none';
   }
 };
-//attach user to the realtime db 
-var user = firebase.auth().currentUser;
-var name, email, photoUrl, uid;
+//attach user to Realtime DB 
+var database = firebase.database();
 
-if (user != null) {
-  name = user.displayName;
-  email = user.email;
-  photoUrl = user.photoURL;
-  uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
-                 // this value to authenticate with your backend server, if
-                 // you have one. Use User.getToken() instead.
-}
 function writeUserData(userId, name, email, imageUrl) {
   firebase.database().ref('users/' + userId).set({
     username: name,
