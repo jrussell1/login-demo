b091679d07e3dbc1625e89649609a8dfda165b20
Jordan Russell <jordanrussell1@gmail.com>
2018-08-09 18:16:11 -0400

    update app.js

--- public/app.js
+++ public/app.js
@@ -217,28 +217,3 @@ var initApp = function() {
 };
 
 window.addEventListener('load', initApp);
-
-      function writeUserData(userId, name, email, imageUrl) {
-        firebase.database().ref('users/' + userId).set({
-          username: name,
-          email: email,
-          profile_picture : imageUrl
-        });
-      }
-      // [END rtdb_write_new_user]
-      
-      function writeUserDataWithCompletion(userId, name, email, imageUrl) {
-        // [START rtdb_write_new_user_completion]
-        firebase.database().ref('users/' + userId).set({
-          username: name,
-          email: email,
-          profile_picture : imageUrl
-        }, function(error) {
-          if (error) {
-            // The write failed...
-          } else {
-            // Data saved successfully!
-          }
-        });
-        // [START rtdb_write_new_user_completion]
-      }
